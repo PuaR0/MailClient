f05545ceaaf976daf09e26462e4a9b79e43148a9
PuaR0 <92799139+PuaR0@users.noreply.github.com>
2022-06-11 02:47:25 +0300

    Initial Commit

---
+++ MailClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MailClient
+//
+//  Created by Алиага С on 11.6.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
